411ddb068a05be53be25c51745f1091a340f5809
Bui Phi Quan <rongxanh2052004@yahoo.com>
2012-11-26 00:37:02 +0700

    gg

--- Resources/ui/handheld/Setting.js
+++ Resources/ui/handheld/Setting.js
@@ -1,6 +1,7 @@
 function Setting() {
 	var self = Ti.UI.createWindow({
 		title: 'Setting',
+		backgroundColor: '#fff'
 	});
 	self.barImage = '/images/handheld/corkboard.jpg';
 	var facebookButton = Titanium.Facebook.createLoginButton({
@@ -10,8 +11,25 @@ function Setting() {
 	var webview = Titanium.UI.createWebView({
 		url: myGlobal.SERVER + '/facebook',
 		width: '100%',
-		height: 200
+		height: 120,
+		bottom: 0
 	});
+	var tbl_data = [];
+	
+	var askForBook = Ti.UI.createTableViewRow({
+		height: 40,
+		id: 0,
+		hasChild: true,
+		title: 'Helps/Ask For Book!'
+	});
+	tbl_data.push(askForBook);
+	var table = Titanium.UI.createTableView({
+    data: tbl_data,
+    height: 'auto',
+    scrollable: false
+    // backgroundImage: '/images/handheld/bookShelf.png',
+	});
+	self.add(table);
 	self.add(webview);
 	return self;
 };
